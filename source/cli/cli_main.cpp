668b5d1e94826bf1114112dad6145535942e102b
Peixuan Zhang <Flower@PoeticFlower.CN>
2018-02-03 09:16:19 +0800

    CLI: Add a very simple OpenCV demo.

--- source/cli/cli_main.cpp
+++ source/cli/cli_main.cpp
@@ -1,6 +1,8 @@
 #include "../common/common.h"
 #include "../common/threading_wrapper.h"
 
+#include <opencv2/opencv.hpp>
+
 class TestThread : public BaseFramework::ThreadingWrapper
 {
 public:
@@ -17,6 +19,21 @@ int main(int argc, char *argv[])
   BaseFramework::ThreadingWrapper *test_thread = new TestThread();
   test_thread->Start();
   test_thread->Stop();
+
+  cv::VideoCapture cap;
+  cv::Mat          img;
+
+  cap.open(0);
+
+  while (1)
+  {
+    cap >> img;
+    if (img.empty())
+      break;
+
+    cv::imshow("Original", img);
+    cv::waitKey(50);
+  }
 
   return 0;
 }
