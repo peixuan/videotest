34816545ce68e7ae7a970f8d5ebe7307441a2cf8
Peixuan Zhang <zhang@poeticflower.cn>
2019-08-29 17:02:03 +0800

    BaseClassLibrary: Add the first version of Timestamp.

---
+++ source/BaseClassLibrary/Timestamp.cpp
@@ -0,0 +1,15 @@
+﻿/*****************************************************************************
+ *
+ * Copyright (C) 2011-2019 Boli Electronic Technology Co., Ltd.
+ *
+ * CONFIDENTIALITY:  This software source code is considered confidential
+ * information. It must be kept confidential in accordance with the terms
+ * and conditions of your Software License Agreement.
+ *
+ *
+ * FILE    : Timestamp.cpp
+ * PURPOSE : Timestamp class used to synchronize various types of  events.
+ * AUTHOR  : Peixuan Zhang <Zhang@PoeticFlower.CN>
+ * COMMENTS: Accurate to nanoseconds.
+ *
+ *****************************************************************************/
