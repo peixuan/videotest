6d45d99629e3dda27b8abf11773a2deb4e4413d4
Peixuan Zhang <Flower@PoeticFlower.CN>
2019-08-29 17:04:03 +0800

    BaseClassLibrary: Add the first version of DatabaseAccess.

---
+++ source/BaseClassLibrary/DatabaseAccess.h
@@ -0,0 +1,20 @@
+﻿/*****************************************************************************
+ *
+ * Copyright (C) 2011-2019 Boli Electronic Technology Co., Ltd.
+ *
+ * CONFIDENTIALITY:  This software source code is considered confidential
+ * information. It must be kept confidential in accordance with the terms
+ * and conditions of your Software License Agreement.
+ *
+ *
+ * FILE    : DatabaseAccess.h
+ * PURPOSE : 
+ * AUTHOR  : Peixuan Zhang <Zhang@PoeticFlower.CN>
+ * COMMENTS: 
+ *
+ *****************************************************************************/
+
+#ifndef   BASE_CLASS_LIBRARY_DATABASE_ACCESS_H_
+#define   BASE_CLASS_LIBRARY_DATABASE_ACCESS_H_
+
+#endif // BASE_CLASS_LIBRARY_DATABASE_ACCESS_H_
