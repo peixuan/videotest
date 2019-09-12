e089c7c844f19b41f49bf26c98945b70cf34206a
Peixuan Zhang <Flower@PoeticFlower.CN>
2019-09-12 08:40:55 +0800

    BaseClassLibrary: Add the first version of PlayerCoreImplFFmpeg.

---
+++ source/BaseClassLibrary/PlayerCoreImplFFmpeg.cpp
@@ -0,0 +1,15 @@
+﻿/*****************************************************************************
+ *
+ * Copyright (C) 2011-2019 Boli Electronic Technology Co., Ltd.
+ *
+ * CONFIDENTIALITY:  This software source code is considered confidential
+ * information. It must be kept confidential in accordance with the terms
+ * and conditions of your Software License Agreement.
+ *
+ *
+ * FILE    : PlayerCoreImplFFmpeg.cpp
+ * PURPOSE : 
+ * AUTHOR  : Peixuan Zhang <Zhang@PoeticFlower.CN>
+ * COMMENTS: 
+ *
+ *****************************************************************************/
