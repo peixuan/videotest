51ea17133e527542281799dcf18e0d2fc2944a93
Peixuan Zhang <zhang@poeticflower.cn>
2019-09-12 08:35:50 +0800

    BaseClassLibrary: Add the first version of PlayerCore.

---
+++ source/BaseClassLibrary/PlayerCore.h
@@ -0,0 +1,20 @@
+﻿/*****************************************************************************
+ *
+ * Copyright (C) 2011-2019 Boli Electronic Technology Co., Ltd.
+ *
+ * CONFIDENTIALITY:  This software source code is considered confidential
+ * information. It must be kept confidential in accordance with the terms
+ * and conditions of your Software License Agreement.
+ *
+ *
+ * FILE    : PlayerCore.h
+ * PURPOSE : Defines the interface for media playback function.
+ * AUTHOR  : Peixuan Zhang <Zhang@PoeticFlower.CN>
+ * COMMENTS: 
+ *
+ *****************************************************************************/
+
+#ifndef   BASE_CLASS_LIBRARY_PLAYER_CORE_H_
+#define   BASE_CLASS_LIBRARY_PLAYER_CORE_H_
+
+#endif // BASE_CLASS_LIBRARY_PLAYER_CORE_H_
